fdea557a7542b072129e20187dc604a06e094fe4
DEV3L <jus.beall@gmail.com>
2024-06-12 11:07:14 -0400

    Add build assistant github action

---
+++ run_build.py
@@ -0,0 +1,39 @@
+from loguru import logger
+
+from src.assistants.assistant_service import (
+    ASSISTANT_NAME,
+    AssistantService,
+)
+from src.clients.openai_api import OpenAIClient, build_openai_client
+from src.exporters.books.books_exporter import BooksExporter
+from src.exporters.files.files_exporter import FilesExporter
+
+
+def export_data():
+    BooksExporter().export()
+    FilesExporter("about.txt").export()
+    FilesExporter("persona.txt").export()
+    FilesExporter("profile.txt").export()
+
+
+def main():
+    logger.info(f"Building {ASSISTANT_NAME}")
+
+    export_data()
+
+    client = OpenAIClient(build_openai_client())
+    service = AssistantService(client)
+
+    logger.info("Removing existing assistant and category files")
+    service.delete_assistant()
+
+    assistant_id = service.get_assistant_id()
+
+    logger.info(f"Assistant ID: {assistant_id}")
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.info(f"Error: {e}")
